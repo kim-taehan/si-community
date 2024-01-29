7f6e2fad548ff6143d958c2dba2365958ee9ec3c
kimtaehan11 <kimtaehan11@gmail.com>
2024-01-29 11:16:18 +0900

    first commit

---
+++ src/main/java/org/devleopx/sicommunity/SiCommunityApplication.java
@@ -0,0 +1,13 @@
+package org.devleopx.sicommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/devleopx/sicommunity/SiCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devleopx.sicommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
